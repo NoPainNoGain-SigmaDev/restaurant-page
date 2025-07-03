d9b859313e83aad3d4b3735f4d8b816d5ac21852
Alejandro Garcia <painisprep@gmail.com>
2025-07-03 15:49:19 -0400

    feat(home): add functionality to order button

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,5 +1,7 @@
 //load home module
 import "../styles/home.css";
+import { clearDOM } from "../modules/dom-control";
+import { loadMenu } from "../modules/menu.js";
 import { createEl, appendElement } from "./dom-control.js";
 
 export function loadHome() {
@@ -14,9 +16,15 @@ export function loadHome() {
         textContent:
           "Descend. Hunt. Feast. In Delicious in Dungeons, every meal is an adventure. Brave your hunger, slay monsters, and taste recipes born in the deepest dark. Join us and dine like a true dungeon explorer!",
       }),
-      createEl("button", { className: "order", textContent: "ORDER NOW" }),
+      createEl("button", { className: "order", textContent: "ORDER NOW", id: "order-btn" }),
     ]),
   ]);
 
   appendElement(homeContent);
+
+  const orderBtn = document.getElementById("order-btn");
+  orderBtn.addEventListener("click", ()=>{
+    clearDOM();
+    loadMenu();
+  });
 }
